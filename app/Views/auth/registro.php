71d1a572800d40b55adc01077bf4a6187fca5b81
App <jose-figueroa.contact@fmail.com>
2026-04-07 12:21:40 +0200

    Crud usuarios: Login, registro, perfiles, roles

---
+++ app/Views/auth/registro.php
@@ -0,0 +1,20 @@
+<h2>Registro</h2>
+
+<form method="POST" action="index.php?action=registro">
+    <input type="text" name="nombre" placeholder="Nombre" required><br>
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Contraseña" required><br>
+
+    <select name="rol">
+        <option value="particular">Cliente</option>
+        <option value="tecnico">Técnico</option>
+        <option value="admin">Admin</option>
+    </select><br>
+
+    <button type="submit">Registrarse</button>
+</form>
+
+<br>
+<a href="index.php">
+    <button type="button">Volver al menú principal</button>
+</a>
---
+++ app/Views/dashboard/cliente.php
@@ -0,0 +1,5 @@
+<h1>Panel CLIENTE</h1>
+<p>Bienvenido a tu área de cliente particular.</p>
+
+<a href="index.php?action=perfil">Editar perfil</a> | 
+<a href="logout.php">Logout</a>
